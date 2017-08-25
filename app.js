5222cff9e394cc11564fe1f2d97e01d333e570bb
jaghit <iamjaghitsingh@gmail.com>
2017-08-25 10:12:53 +0530

    Create folder for new user; /new url

--- app.js
+++ app.js
@@ -8,23 +8,16 @@ const path = require('path');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-app.post('/upload', (req, res) => {
-    var form = new multiparty.Form();
-    form.parse(req, function (err, fields, files) {
-        let oldPath = files.file[0].path;
-        let newPath = path.join('./Public/' + fields.path[0] + files.file[0].originalFilename);
-        if (!fs.existsSync(newPath)) {
-            fs.rename(oldPath, newPath, (err) => {
-                if (err) throw err;
-                res.status(201);
-                res.send();
-            });
-        } else {
-            res.status(200);
-            res.send();
-        }
-
-    });
+app.post('/new', (req, res) => {
+    let dir = path.join('./Public/' + req.body.name);
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+        res.status(201);
+        res.json({name: req.body.name, exists: false});
+    } else {
+        res.status(200);
+        res.json({name: req.body.name, exists: true});
+    }
 });
 
 app.post('/list', (req, res) => {
@@ -45,15 +38,34 @@ app.post('/list', (req, res) => {
     res.json(body);
 });
 
+app.post('/upload', (req, res) => {
+    var form = new multiparty.Form();
+    form.parse(req, function (err, fields, files) {
+        let oldPath = files.file[0].path;
+        let newPath = path.join('./Public/' + fields.path[0] + files.file[0].originalFilename);
+        if (!fs.existsSync(newPath)) {
+            fs.rename(oldPath, newPath, (err) => {
+                if (err) throw err;
+                res.status(201);
+                res.json({file: files.file[0].originalFilename, exists: false});
+            });
+        } else {
+            res.status(200);
+            res.json({file: files.file[0].originalFilename, exists: true});
+        }
+
+    });
+});
+
 app.post('/create', (req, res) => {
     let dir = path.join('./Public/' + req.body.path + req.body.name);
     if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
         res.status(201);
-        res.send();
+        res.json({folder: req.body.name, exists: false});
     } else {
         res.status(200);
-        res.send();
+        res.json({folder: req.body.name, exists: true});
     }
 });
 
