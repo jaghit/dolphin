c6108911be8af0177824d53755ba4656a0531dbb
jaghit <iamjaghitsingh@gmail.com>
2017-08-25 17:03:32 +0530

    Install cors, log all endpoints, change APIs responses & new endpoint /delete, change res from send to json

--- app.js
+++ app.js
@@ -6,89 +6,133 @@ const app = express();
 const multiparty = require('multiparty');
 const fs = require('fs');
 const path = require('path');
-const cors = require('cors');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 
+const cors = require('cors');
 app.use(cors());
 
-app.post('/new', (req, res) => {
-    let dd = new Date();
-    let logTime = '[' + dd.getHours() + ':' + dd.getMinutes() + ':' + dd.getSeconds() + '] ';
-    console.log(logTime + 'New request to /new endpoint');
+function log(req, point) {
+    let timestamp = new Date();
+    let agent = req.headers['user-agent'];
+    let address = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    let log = '\n[ TIMESTAMP ] ' + timestamp + '\n[ ENDPOINT ] Incoming request to ' + point + ' \n[ USERAGENT ] ' + agent + '\n[ IP ADDRESS ] ' + address;
+    console.log(log);
+}
 
-    let dir = path.join('./Public/' + req.body.name);
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
+app.post('/new', (req, res) => {
+    log(req, '/New');
+
+    let _path = path.join('./Public/' + req.body.name + '/');
+    if (!fs.existsSync(_path)) {
+        fs.mkdirSync(_path);
         res.status(201);
-        res.json({ name: req.body.name, exists: false });
+        res.json({ exist: false, create: true });
     } else {
         res.status(200);
-        res.send('{ name: ' + req.body.name + ', exists: false }');
+        res.json({ exist: true, create: false });
     }
 });
 
 app.post('/list', (req, res) => {
-    let dd = new Date();
-    let logTime = '[' + dd.getHours() + ':' + dd.getMinutes() + ':' + dd.getSeconds() + '] ';
-    console.log(logTime + 'New request to /list endpoint');
+    log(req, '/List');
 
-    let dir = path.join('./Public/' + req.body.path);
-    var body = {
+    let _path = path.join('./Public/' + req.body.path + '/');
+    let body = {
         folders: [],
         files: []
     };
-    let files = fs.readdirSync(dir);
-    for (let i = 0; i < files.length; i++) {
-        if (fs.lstatSync(dir + '/' + files[i]).isDirectory()) {
-            body.folders.push(files[i]);
-        } else {
-            body.files.push(files[i]);
+    if (fs.existsSync(_path)) {
+        let files = fs.readdirSync(_path);
+        for (let i = 0; i < files.length; i++) {
+            if (fs.lstatSync(_path + '/' + files[i]).isDirectory()) {
+                body.folders.push(files[i]);
+            } else {
+                body.files.push(files[i]);
+            }
         }
+        res.status(200);
+        res.json(body);
+    } else {
+        res.status(404);
+        res.json({ exist: false });
     }
-    res.status(200);
-    res.json(body);
+
 });
 
 app.post('/upload', (req, res) => {
-    let dd = new Date();
-    let logTime = '[' + dd.getHours() + ':' + dd.getMinutes() + ':' + dd.getSeconds() + '] ';
-    console.log(logTime + 'New request to /upload endpoint');
+    log(req, '/Upload');
 
     var form = new multiparty.Form();
     form.parse(req, function (err, fields, files) {
         let oldPath = files.file[0].path;
-        let newPath = path.join('./Public/' + fields.path[0] + files.file[0].originalFilename);
-        if (!fs.existsSync(newPath)) {
-            fs.rename(oldPath, newPath, (err) => {
-                if (err) throw err;
-                res.status(201);
-                res.json({ file: files.file[0].originalFilename, exists: false });
-            });
+        let newPath = path.join('./Public/' + fields.path[0] + '/');
+        if (fs.existsSync(newPath)) {
+            newPath = path.join(newPath + '/' + files.file[0].originalFilename);
+            if (!fs.existsSync(newPath)) {
+                fs.rename(oldPath, newPath, (err) => {
+                    if (err) throw err;
+                    res.status(201);
+                    res.json({ exist: false, create: true });
+                });
+            } else {
+                res.status(200);
+                res.json({ exist: true, create: false });
+            }
         } else {
-            res.status(200);
-            res.json({ file: files.file[0].originalFilename, exists: true });
+            res.status(404);
+            res.json({ exists: false });
         }
-
     });
 });
 
 app.post('/create', (req, res) => {
-    let dd = new Date();
-    let logTime = '[' + dd.getHours() + ':' + dd.getMinutes() + ':' + dd.getSeconds() + '] ';
-    console.log(logTime + 'New request to /create endpoint');
+    log(req, '/Create');
 
-    let dir = path.join('./Public/' + req.body.path + req.body.name);
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
-        res.status(201);
-        res.json({ folder: req.body.name, exists: false });
+    let _path = path.join('./Public/' + req.body.path + '/');
+    if (fs.existsSync(_path)) {
+        _path = path.join(_path + '/' + req.body.name)
+        if (!fs.existsSync(_path)) {
+            fs.mkdirSync(_path);
+            res.status(201);
+            res.json({ exist: false, create: true });
+        } else {
+            res.status(200);
+            res.json({ exist: true, create: false });
+        }
     } else {
-        res.status(200);
-        res.json({ folder: req.body.name, exists: true });
+        res.status(404);
+        res.json({ exist: false, create: false });
     }
 });
+
+app.post('/delete', (req, res) => {
+    log(req, '/Delete');
+
+    let _path = path.join('./Public/' + req.body.path);
+    let deleted = removeRecursive(_path);
+    res.status(200);
+    res.json({ exist: deleted, delete: deleted });
+});
+
+function removeRecursive(_path) {
+    if (fs.existsSync(_path)) {
+        fs.readdirSync(_path).forEach(function (file, index) {
+            var curPath = _path + "/" + file;
+            if (fs.lstatSync(curPath).isDirectory()) {
+                removeRecursive(curPath);
+            } else {
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(_path);
+        return true;
+    } else {
+        return false;
+    }
+};
 
 app.listen(8080, () => {
     console.log('CORS-enabled web server listening on port 8080!');
